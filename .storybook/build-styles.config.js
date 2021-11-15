8412939a65a0f5ee203c43d13cd163fa1b279416
andy <andreyelpaev@gmail.com>
2021-11-15 18:00:10 +0500

    update storybook to 6.4.0-rc1

---
+++ .storybook/build-styles.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: path.resolve(__dirname) + '/styles.scss',
+  output: {
+    path: path.resolve(__dirname, 'assets'),
+  },
+  plugins: [new MiniCssExtractPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+};
