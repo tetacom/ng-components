04427d78029547d0d5ba1e065626d0cf1f2f1b21
fclm.man <fclm.man@gmail.com>
2022-02-04 13:27:48 +0500

    t
    t

--- projects/chart/src/public-api.ts
+++ projects/chart/src/public-api.ts
@@ -3,7 +3,9 @@
  */
 
 export * from './chart/chart.module';
-export * from './chart/service/chart.service';
+export * from './chart/service/public-api';
+export * from './chart/base/series-base.component';
 export * from './chart/chart/chart.component';
 export * from './chart/model/public-api';
 export * from './chart/chart-container/series/public-api';
+export * from './chart/core/axis/axis';
