db65a8c6a20bd48c91abd0b7c859bf11ce69c23e
fclm.man <fclm.man@gmail.com>
2022-02-10 10:07:24 +0500

    t

--- projects/chart/src/chart/chart-container/series/block-series/block-series.component.ts
+++ projects/chart/src/chart/chart-container/series/block-series/block-series.component.ts
@@ -42,7 +42,7 @@ export class BlockSeriesComponent<T extends BasePoint>
   }
 
   override ngOnInit(): void {
-    const defaultVisiblePixels = 3;
+    const defaultVisiblePixels = 1;
 
     this.x = this.scaleService.xScaleMap.pipe(
       map((_) => _.get(this.series.xAxisIndex))
