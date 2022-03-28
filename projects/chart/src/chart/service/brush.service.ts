06a9a02caea75ae9c2cd71236b21b9259091e61d
Andrey Elpaev <andreyelpaev@gmail.com>
2022-03-28 18:16:56 +0500

    fix: add touch event to brush tool

--- projects/chart/src/chart/service/brush.service.ts
+++ projects/chart/src/chart/service/brush.service.ts
@@ -1,12 +1,16 @@
-import {ElementRef, Injectable, NgZone} from '@angular/core';
-import {BrushType} from '../model/enum/brush-type';
+import { ElementRef, Injectable, NgZone } from '@angular/core';
+import { BrushType } from '../model/enum/brush-type';
 import * as d3 from 'd3';
-import {animationFrameScheduler, filter, Subscription, tap} from 'rxjs';
-import {BroadcastService} from './broadcast.service';
-import {IChartConfig} from '../model/i-chart-config';
-import {BrushMessage, IBroadcastMessage, ZoomMessage} from '../model/i-broadcast-message';
-import {throttleTime} from 'rxjs/operators';
-import {AxisOrientation} from "../model/enum/axis-orientation";
+import { animationFrameScheduler, filter, Subscription, tap } from 'rxjs';
+import { BroadcastService } from './broadcast.service';
+import { IChartConfig } from '../model/i-chart-config';
+import {
+  BrushMessage,
+  IBroadcastMessage,
+  ZoomMessage,
+} from '../model/i-broadcast-message';
+import { throttleTime } from 'rxjs/operators';
+import { AxisOrientation } from '../model/enum/axis-orientation';
 
 @Injectable({
   providedIn: 'root',
@@ -22,15 +26,12 @@ export class BrushService {
 
   private selection: number[];
 
+  constructor(
+    private broadcastService: BroadcastService,
+    private zone: NgZone
+  ) {}
 
-  constructor(private broadcastService: BroadcastService, private zone: NgZone) {
-  }
-
-  applyBrush(
-    svgElement: ElementRef,
-    config: IChartConfig,
-    brushScale: any
-  ) {
+  applyBrush(svgElement: ElementRef, config: IChartConfig, brushScale: any) {
     this.broadcastSubscribtion?.unsubscribe();
     this.brush?.on('start brush end', null);
     if (config.brush?.enable) {
@@ -45,35 +46,80 @@ export class BrushService {
           const [from, to] = _.selection as number[];
 
           if (to - from === 0) {
-            const selection: number[] = this.selection?.map(brushScale) ?? [config.brush?.from, config.brush?.to].map(brushScale);
+            const selection: number[] =
+              this.selection?.map(brushScale) ??
+              [config.brush?.from, config.brush?.to].map(brushScale);
             const halfBrushHeight = (selection[1] - selection[0]) / 2;
 
-            const invertedSelection: number[] = [from - halfBrushHeight, to + halfBrushHeight].map(brushScale.invert)
+            const invertedSelection: number[] = [
+              from - halfBrushHeight,
+              to + halfBrushHeight,
+            ].map(brushScale.invert);
 
-            if(invertedSelection[1] - invertedSelection[0] > config.brush?.max) {
-              container.call(this.brush.move, [Math.floor(invertedSelection[0]), Math.floor(invertedSelection[0] + config.brush?.max)].map(brushScale));
+            if (
+              invertedSelection[1] - invertedSelection[0] >
+              config.brush?.max
+            ) {
+              container.call(
+                this.brush.move,
+                [
+                  Math.floor(invertedSelection[0]),
+                  Math.floor(invertedSelection[0] + config.brush?.max),
+                ].map(brushScale)
+              );
               return;
             }
 
-            if(invertedSelection[1] - invertedSelection[0] < config.brush?.min) {
-              container.call(this.brush.move, [Math.floor(invertedSelection[0]), Math.ceil(invertedSelection[0] + config.brush?.min)].map(brushScale));
+            if (
+              invertedSelection[1] - invertedSelection[0] <
+              config.brush?.min
+            ) {
+              container.call(
+                this.brush.move,
+                [
+                  Math.floor(invertedSelection[0]),
+                  Math.ceil(invertedSelection[0] + config.brush?.min),
+                ].map(brushScale)
+              );
               return;
             }
 
-            container.call(this.brush.move, [from - halfBrushHeight, to + halfBrushHeight]);
+            container.call(this.brush.move, [
+              from - halfBrushHeight,
+              to + halfBrushHeight,
+            ]);
             return;
           }
 
-
-
-          if (brushScale.invert(to) - brushScale.invert(from) > config.brush?.max) {
-            container.call(this.brush.move, this.selection ? [this.selection[0], this.selection[0] + config.brush?.max].map(brushScale) : [config.brush?.from, config.brush?.to].map(brushScale));
+          if (
+            brushScale.invert(to) - brushScale.invert(from) >
+            config.brush?.max
+          ) {
+            container.call(
+              this.brush.move,
+              this.selection
+                ? [
+                    this.selection[0],
+                    this.selection[0] + config.brush?.max,
+                  ].map(brushScale)
+                : [config.brush?.from, config.brush?.to].map(brushScale)
+            );
             return;
           }
 
-          if (brushScale.invert(to) - brushScale.invert(from) < config.brush?.min) {
-
-            container.call(this.brush.move, this.selection ? [this.selection[0], this.selection[0] + config.brush?.min].map(brushScale) : [config.brush?.from, config.brush?.to].map(brushScale));
+          if (
+            brushScale.invert(to) - brushScale.invert(from) <
+            config.brush?.min
+          ) {
+            container.call(
+              this.brush.move,
+              this.selection
+                ? [
+                    this.selection[0],
+                    this.selection[0] + config.brush?.min,
+                  ].map(brushScale)
+                : [config.brush?.from, config.brush?.to].map(brushScale)
+            );
             return;
           }
 
@@ -85,7 +131,7 @@ export class BrushService {
             event: _,
             selection: [brushScale.invert(from), brushScale.invert(to)],
             brushType: config?.brush?.type ?? BrushType.x,
-            brushScale
+            brushScale,
           });
 
           this.broadcastService.broadcastBrush({
@@ -95,14 +141,11 @@ export class BrushService {
         }
       });
 
-
-
       this.zone.runOutsideAngular(() => {
         setTimeout(() => {
           container.call(this.brush);
 
           let domain = brushScale.domain();
-
 
           if (config?.brush?.from) {
             domain[0] = config.brush.from;
@@ -112,49 +155,64 @@ export class BrushService {
             domain[1] = config.brush.to;
           }
 
-          container.call(this.brush.move, this.selection ? this.selection.map(brushScale) : domain.map(brushScale), {});
-
+          container.call(
+            this.brush.move,
+            this.selection
+              ? this.selection.map(brushScale)
+              : domain.map(brushScale),
+            {}
+          );
         }, 0);
-      })
+      });
 
+      this.broadcastSubscribtion = this.broadcastService
+        .subscribeToZoom(config?.zoom?.syncChannel)
+        .pipe(
+          filter((m: IBroadcastMessage<ZoomMessage>) => {
+            return (
+              m.message.event.sourceEvent instanceof MouseEvent ||
+              m.message.event.sourceEvent instanceof WheelEvent ||
+              (window.TouchEvent &&
+                m.message.event.sourceEvent instanceof TouchEvent)
+            );
+          }),
+          throttleTime(0, animationFrameScheduler, { trailing: true }),
+          tap((m: IBroadcastMessage<ZoomMessage>) => {
+            const {
+              message: { brushDomain },
+            } = m;
 
+            if (
+              (m.message?.axis.index === 0 &&
+                m.message?.axis.orientation === AxisOrientation.y &&
+                config.brush?.type === BrushType.y) ||
+              (m.message?.axis.orientation === AxisOrientation.x &&
+                config.brush?.type === BrushType.x)
+            ) {
+              container.call(this.brush.move, [
+                brushScale(brushDomain[0]),
+                brushScale(brushDomain[1]),
+              ]);
 
-      this.broadcastSubscribtion = this.broadcastService.subscribeToZoom(config?.zoom?.syncChannel).pipe(
-        filter((m: IBroadcastMessage<ZoomMessage>) => {
-          return m.message.event.sourceEvent instanceof MouseEvent || m.message.event.sourceEvent instanceof WheelEvent;
-        }),
-        throttleTime(0, animationFrameScheduler, {trailing: true}),
-        tap((m: IBroadcastMessage<ZoomMessage>) => {
-          const {message: {brushDomain}} = m;
+              if (m.message.event.type === 'end') {
+                const brushMessage = new BrushMessage({
+                  event: null,
+                  selection: brushDomain,
+                  brushType: config?.brush?.type ?? BrushType.x,
+                  brushScale,
+                });
 
-          if(m.message?.axis.index === 0 && m.message?.axis.orientation === AxisOrientation.y && config.brush?.type === BrushType.y || m.message?.axis.orientation === AxisOrientation.x && config.brush?.type === BrushType.x) {
+                this.broadcastService.broadcastBrush({
+                  channel: config?.zoom?.syncChannel,
+                  message: brushMessage,
+                });
+              }
 
-            container.call(this.brush.move, [
-              brushScale(brushDomain[0]),
-              brushScale(brushDomain[1]),
-            ]);
-
-            if (m.message.event.type === 'end') {
-              const brushMessage = new BrushMessage({
-                event: null,
-                selection: brushDomain,
-                brushType: config?.brush?.type ?? BrushType.x,
-                brushScale,
-              });
-
-              this.broadcastService.broadcastBrush({
-                channel: config?.zoom?.syncChannel,
-                message: brushMessage,
-              });
+              this.selection = brushDomain;
             }
-
-            this.selection = brushDomain;
-          }
-
-
-
-        })
-      ).subscribe();
+          })
+        )
+        .subscribe();
     }
   }
 
