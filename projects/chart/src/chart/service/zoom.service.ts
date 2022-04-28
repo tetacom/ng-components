55b7b8d6a26947c8d6a1469882eea1dc5d96a41c
andy <andreyelpaev@gmail.com>
2022-04-28 09:45:02 +0500

    fix: cleanup

--- projects/chart/src/chart/service/zoom.service.ts
+++ projects/chart/src/chart/service/zoom.service.ts
@@ -11,14 +11,12 @@ import objectHash from 'object-hash';
   providedIn: 'root',
 })
 export class ZoomService {
-
   broadcastSubscription = [];
   zoomed: Observable<IChartEvent<Axis>>;
   axisHashMap = new Map<string, Axis>();
   scaleHashMap = new Map<string, any>();
   elementHashMap = new Map<string, any>();
   zoomHashMap = new Map<string, ZoomBehavior<any, any>>();
-
 
   private zoomed$ = new BehaviorSubject<IChartEvent<Axis>>(null);
   private broadcastChannel: string;
@@ -39,13 +37,10 @@ export class ZoomService {
   }
 
   setZoom(from: number, to: number, axisIndex = 0, axisOrientation = AxisOrientation.x) {
-
     const hash = objectHash.sha1({index: axisIndex, orientation: axisOrientation});
-
     if (!this.zoomHashMap.has(hash)) {
       return;
     }
-
     const currentAxis = this.axisHashMap.get(hash);
     const currentScale = this.scaleHashMap.get(hash)?.copy();
     const currentElement = this.elementHashMap.get(hash);
@@ -67,43 +62,32 @@ export class ZoomService {
 
     const bBox = currentElement.node().getBBox();
     const delta = Math.abs(currentScale(to) - currentScale(from));
-
     const scale = axisOrientation === AxisOrientation.x ? bBox.width / delta : bBox.height / delta;
-
     let transform = zoomIdentity.scale(scale);
 
     if (currentAxis.orientation === AxisOrientation.x) {
-
       if (currentAxis.options.scaleType.type === ScaleType.log) {
         currentScale.domain(currentAxis.options.inverted ? [...currentScale.domain()].reverse() : currentScale.domain())
       }
-
       transform = transform.translate(-currentScale(from), 0);
     }
 
     if (currentAxis.orientation === AxisOrientation.y) {
-
       if (currentAxis.options.scaleType.type === ScaleType.log) {
         currentScale.domain(currentAxis.options.inverted ? currentScale.domain() : [...currentScale.domain()].reverse())
       }
-
       transform = transform.translate(0, -currentScale(from));
     }
-
     currentElement.transition().call(currentZoom.transform, transform, null, new MouseEvent('setZoom'))
   }
 
   resetZoom(axisIndex = 0, axisOrientation = AxisOrientation.x) {
-
     const hash = objectHash.sha1({index: axisIndex, orientation: axisOrientation});
-
     if (!this.zoomHashMap.has(hash)) {
       return;
     }
-
     const currentElement = this.elementHashMap.get(hash);
     const currentZoom = this.zoomHashMap.get(hash);
-
     currentElement.transition().call(currentZoom.transform, zoomIdentity, null, new MouseEvent('resetZoom'));
   }
 
