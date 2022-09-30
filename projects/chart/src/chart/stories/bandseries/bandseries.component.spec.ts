8640e2f7b290e24931fb1f22742afc24a3e9d698
Andrey Mikhailovskii <andreyelpaev@gmail.com>
2022-09-30 08:34:59 +0000

    fix: add band scale

---
+++ projects/chart/src/chart/stories/bandseries/bandseries.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BandseriesComponent } from './bandseries.component';
+
+describe('BandseriesComponent', () => {
+  let component: BandseriesComponent;
+  let fixture: ComponentFixture<BandseriesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ BandseriesComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(BandseriesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
