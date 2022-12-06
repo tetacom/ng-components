d6ca19d032bc0da8f5dcbc0a2202797ca8d2e521
andy <andreyelpaev@gmail.com>
2022-12-06 10:21:31 +0500

    theme switcher

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,19 +1,19 @@
 module.exports = {
-  'stories': [
-    '../projects/components/**/*.stories.mdx',
-    '../projects/components/**/*.stories.@(js|jsx|ts|tsx|mdx)',
-    '../projects/chart/**/*.stories.mdx',
-    '../projects/chart/**/*.stories.@(js|jsx|ts|tsx|mdx)',
+  stories: [
+    "../projects/components/**/*.stories.mdx",
+    "../projects/components/**/*.stories.@(js|jsx|ts|tsx|mdx)",
+    "../projects/chart/**/*.stories.mdx",
+    "../projects/chart/**/*.stories.@(js|jsx|ts|tsx|mdx)",
   ],
-  'addons': [
-    '@storybook/addon-knobs',
-    '@storybook/addon-docs',
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    'storybook-addon-themes'
+  addons: [
+    "@storybook/addon-knobs",
+    "@storybook/addon-docs",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "storybook-addon-theme-switcher",
   ],
-  'framework': '@storybook/angular',
-  'core': {
-    'builder': 'webpack5'
-  }
-}
+  framework: "@storybook/angular",
+  core: {
+    builder: "webpack5",
+  },
+};
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,30 +1,27 @@
-import {setCompodocJson} from '@storybook/addon-docs/angular';
-import docJson from '../compodoc/documentation.json';
-import {addDecorator, moduleMetadata} from '@storybook/angular';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {addons} from '@storybook/addons';
+import { setCompodocJson } from "@storybook/addon-docs/angular";
+import docJson from "../compodoc/documentation.json";
+import { addDecorator, moduleMetadata } from "@storybook/angular";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { addons } from "@storybook/addons";
 
 addons.setConfig({
-  enableShortcuts: false
+  enableShortcuts: false,
 });
 
 setCompodocJson(docJson);
 
 export const parameters = {
-  actions: {argTypesRegex: '^on[A-Z].*'},
+  actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-  docs: {inlineStories: true},
-  themes: {
-    default: 'default',
-    list: [
-      {name: 'dark', class: 'theme-dark', color: '#000000'},
-      {name: 'default', class: 'theme-default', color: '#ffffff'}
-    ],
+  docs: { inlineStories: true },
+  themeSwitcher: {
+    themes: ["baselight", "basedark", "purplelight", "purpledark"],
+    dataAttribute: "theme",
   },
-}
-addDecorator(moduleMetadata({imports: [BrowserAnimationsModule]}));
+};
+addDecorator(moduleMetadata({ imports: [BrowserAnimationsModule] }));
