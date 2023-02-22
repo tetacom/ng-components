c2e6c9ef978306bccc0abf725e9b0add5cb50d01
martnemov <martnemov@ya.ru>
2023-02-22 15:10:21 +0500

    fix: add file-icon component

---
+++ projects/components/src/component/icon/icon-file/icon-file.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { IconFileComponent } from './icon-file.component';
+
+describe('IconFileComponent', () => {
+  let component: IconFileComponent;
+  let fixture: ComponentFixture<IconFileComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [IconFileComponent]
+    })
+      .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(IconFileComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- projects/components/src/component/icon/public-api.ts
+++ projects/components/src/component/icon/public-api.ts
@@ -2,3 +2,4 @@ export * from './icon.module';
 export * from './icon.service';
 export * from './icon-sprite.directive';
 export * from './icon/icon.component';
+export * from './icon-file/icon-file.component'
