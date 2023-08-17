f88f8d97c7294e54599deab08910144638b5b53e
Bulat.V <fclm.man@gmail.com>
2023-08-17 19:21:51 +0400

    fix selector

--- projects/chart/src/chart/chart-container/series/block-area-series/block-area-series.component.ts
+++ projects/chart/src/chart/chart-container/series/block-area-series/block-area-series.component.ts
@@ -6,16 +6,17 @@ import {
   ElementRef,
   OnInit,
 } from '@angular/core';
+import { filter, map, Observable } from 'rxjs';
+
 import { SeriesBaseComponent } from '../../../base/series-base.component';
 import { BasePoint } from '../../../model/base-point';
+import { FillType } from '../../../model/enum/fill-type';
 import { ChartService } from '../../../service/chart.service';
 import { ScaleService } from '../../../service/scale.service';
 import { ZoomService } from '../../../service/zoom.service';
-import { filter, map, Observable } from 'rxjs';
-import { FillType } from '../../../model/enum/fill-type';
 
 @Component({
-  selector: 'svg:svg[teta-block-area-3d-series]',
+  selector: 'svg:svg[teta-block-area-series]',
   templateUrl: './block-area-series.component.html',
   styleUrls: ['./block-area-series.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -43,15 +44,15 @@ export class BlockAreaSeriesComponent<T extends BasePoint>
 
   override ngOnInit(): void {
     this.x = this.scaleService.scales.pipe(
-      map((_) => _.x.get(this.series.xAxisIndex)?.scale)
+      map(_ => _.x.get(this.series.xAxisIndex)?.scale)
     );
     this.y = this.scaleService.scales.pipe(
-      map((_) => _.y.get(this.series.yAxisIndex)?.scale)
+      map(_ => _.y.get(this.series.yAxisIndex)?.scale)
     );
 
     this.displayPoints = this.y.pipe(
-      filter((y) => y),
-      map((y) => {
+      filter(y => y),
+      map(y => {
         return this.series.data.filter((point, index, arr) => {
           const [min, max] = y.domain();
           return (
