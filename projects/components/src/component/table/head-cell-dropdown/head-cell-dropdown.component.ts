0951a257cdac4ce270e4084f354fe522c4a0a3e2
Bulat.V <fclm.man@gmail.com>
2023-09-11 13:09:14 +0400

    fix: dropdown

--- projects/components/src/component/table/head-cell-dropdown/head-cell-dropdown.component.ts
+++ projects/components/src/component/table/head-cell-dropdown/head-cell-dropdown.component.ts
@@ -4,16 +4,15 @@ import {
   Component,
   EventEmitter,
   Input,
-  OnDestroy,
-  OnInit,
   Output,
 } from '@angular/core';
-import {TableService} from '../service/table.service';
-import {TableColumn} from '../contract/table-column';
-import {FilterState} from '../../filter/contarct/filter-state';
-import {ITreeData} from '../../../common/contract/i-tree-data';
-import {TetaConfigService} from '../../../locale/teta-config.service';
-import {HeadDropdownTab} from '../contract/head-dropdown-tab';
+
+import { ITreeData } from '../../../common/contract/i-tree-data';
+import { TetaConfigService } from '../../../locale/teta-config.service';
+import { FilterState } from '../../filter/contarct/filter-state';
+import { HeadDropdownTab } from '../contract/head-dropdown-tab';
+import { TableColumn } from '../contract/table-column';
+import { TableService } from '../service/table.service';
 
 @Component({
   selector: 'teta-head-cell-dropdown',
@@ -21,7 +20,7 @@ import {HeadDropdownTab} from '../contract/head-dropdown-tab';
   styleUrls: ['./head-cell-dropdown.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeadCellDropdownComponent<T> implements OnInit, OnDestroy {
+export class HeadCellDropdownComponent<T> {
   @Input() columns: ITreeData[];
   @Input() column: TableColumn;
   @Input() state: FilterState;
@@ -31,21 +30,16 @@ export class HeadCellDropdownComponent<T> implements OnInit, OnDestroy {
   @Output() dropDownOpenChange: EventEmitter<boolean> =
     new EventEmitter<boolean>();
 
-  constructor(private _svc: TableService<T>,
-              private _config: TetaConfigService,
-              private _cdr: ChangeDetectorRef) {
-  }
+  constructor(
+    private _svc: TableService<T>,
+    private _config: TetaConfigService,
+    private _cdr: ChangeDetectorRef
+  ) {}
 
   closeDropdown = () => {
     this.dropDownOpen = false;
     this.dropDownOpenChange.emit(false);
   };
-
-  ngOnInit(): void {
-  }
-
-  ngOnDestroy(): void {
-  }
 
   trackIndex(index) {
     return index;
