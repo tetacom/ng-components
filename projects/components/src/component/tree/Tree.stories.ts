40517b64bf715fefb6e22dba373b640376ff8e82
эдуард хасанов <2506505q@mail.ru>
2023-09-29 12:14:08 +0500

    fix: add docs

--- projects/components/src/component/tree/Tree.stories.ts
+++ projects/components/src/component/tree/Tree.stories.ts
@@ -1,102 +1,119 @@
-import {withKnobs} from '@storybook/addon-knobs';
-import {TreeComponent} from './tree/tree.component';
-import {TreeModule} from './tree.module';
-import {TetaTemplateModule} from '../../directive/teta-template/teta-template.module';
-import {IconModule} from '../icon/icon.module';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {applicationConfig, Meta} from "@storybook/angular";
-import {importProvidersFrom} from "@angular/core";
-import {HttpClientModule} from "@angular/common/http";
+import { withKnobs } from '@storybook/addon-knobs';
+import { TreeComponent } from './tree/tree.component';
+import { TreeModule } from './tree.module';
+import { TetaTemplateModule } from '../../directive/teta-template/teta-template.module';
+import { IconModule } from '../icon/icon.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { applicationConfig, Meta } from '@storybook/angular';
+import { importProvidersFrom } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 export default {
   title: 'Component/Tree',
   decorators: [
     withKnobs,
     applicationConfig({
-      providers: [
-        importProvidersFrom(HttpClientModule)
-      ],
+      providers: [importProvidersFrom(HttpClientModule)],
     }),
   ],
-  component: TreeComponent
+  component: TreeComponent,
 } as Meta;
 
 export const basicTree = () => ({
   moduleMetadata: {
-    imports: [TreeModule, TetaTemplateModule, IconModule, BrowserAnimationsModule]
+    imports: [
+      TreeModule,
+      TetaTemplateModule,
+      IconModule,
+      BrowserAnimationsModule,
+    ],
   },
   props: {
-    data: treeData
+    data: treeData,
   },
-  template: `<teta-tree [tetaIconSprite]="'assets/icons.svg'" style="width: 400px;" [data]="data">
+  template: `<teta-tree  [tetaIconSprite]="'assets/icons.svg'" style="width: 400px;" [data]="data">
   <ng-template tetaTemplate let-item>
     <teta-icon [name]="item.icon" class="fill-text-30 margin-right-2"></teta-icon> {{item.name}}
   </ng-template>
 </teta-tree>`,
 });
 
-const treeData = [{
-  name: 'Тетакомовское м-р (4)',
-  icon: 'layers',
-  children: [
-    {
-      name: 'Скважины для тестирования',
-      icon: 'folder',
-      children: [
-        {
-          name: 'Тестовая скважина №1',
-          icon: 'well',
-          children: []
-        }, {
-          name: 'Стресс-тест',
-          icon: 'well',
-          children: []
-        }
-      ]
-    }
-  ]
-}, {
-  name: 'Test only (1)',
-  icon: 'layers',
-  children: [
-    {
-      name: 'Group (1)',
-      icon: 'folder',
-      children: [
-        {
-          name: 'well',
-          icon: 'well',
-          children: [{
-            name: 'ГИС',
-            icon: 'map',
-            children: []
-          }, {
-            name: 'Конструкция',
-            icon: 'tubes',
-            children: []
-          }, {
-            name: 'D1',
-            icon: 'angle',
-            children: []
-          }, {
-            name: 'Дизайн №1 copy',
-            icon: 'angle',
-            children: [{
-              name: 'Зоны продуктивности',
-              children: []
-            }, {
-              name: 'План закачки',
-              children: []
-            }, {
-              name: 'Опции',
-              children: []
-            }, {
-              name: 'Результаты',
-              children: []
-            }]
-          }]
-        }
-      ]
-    }
-  ]
-}];
+const treeData = [
+  {
+    name: 'Тетакомовское м-р (4)',
+    icon: 'layers',
+    children: [
+      {
+        name: 'Скважины для тестирования',
+        icon: 'folder',
+        children: [
+          {
+            name: 'Тестовая скважина №1',
+            icon: 'well',
+            children: [],
+          },
+          {
+            name: 'Стресс-тест',
+            icon: 'well',
+            children: [],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: 'Test only (1)',
+    icon: 'layers',
+    children: [
+      {
+        name: 'Group (1)',
+        icon: 'folder',
+        children: [
+          {
+            name: 'well',
+            icon: 'well',
+            children: [
+              {
+                name: 'ГИС',
+                icon: 'map',
+                children: [],
+              },
+              {
+                name: 'Конструкция',
+                icon: 'tubes',
+                children: [],
+              },
+              {
+                name: 'D1',
+                icon: 'angle',
+                children: [],
+              },
+              {
+                name: 'Дизайн №1 copy',
+                icon: 'angle',
+                children: [
+                  {
+                    name: 'Зоны продуктивности',
+                    children: [],
+                  },
+                  {
+                    name: 'План закачки',
+                    children: [],
+                  },
+                  {
+                    name: 'Опции',
+                    children: [],
+                  },
+                  {
+                    name: 'Результаты',
+                    children: [],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+];
