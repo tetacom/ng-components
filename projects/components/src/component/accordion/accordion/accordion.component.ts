a61f41da4737f7db58ddc0e9b5938e891c4421d0
Bulat.V <fclm.man@gmail.com>
2024-02-27 16:10:16 +0400

    feat: version

--- projects/components/src/component/accordion/accordion/accordion.component.ts
+++ projects/components/src/component/accordion/accordion/accordion.component.ts
@@ -1,11 +1,11 @@
-import { ChangeDetectionStrategy, Component, HostBinding } from '@angular/core';
+import {ChangeDetectionStrategy, Component, HostBinding} from '@angular/core';
 
 @Component({
-    selector: 'teta-accordion',
-    templateUrl: './accordion.component.html',
-    styleUrls: ['./accordion.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    standalone: true,
+  selector: 'teta-accordion',
+  templateUrl: './accordion.component.html',
+  styleUrls: ['./accordion.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
 })
 export class AccordionComponent {
   @HostBinding('class.accordion') private readonly accordionClassName = true;
