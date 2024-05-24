4748e865d540d6a81e45c74ef595e116091db299
Marat Sarvarov <shtirlizc@gmail.com>
2024-05-24 15:59:00 +0500

    fix: update accordion docs

--- projects/components/src/component/accordion/Accordion.stories.ts
+++ projects/components/src/component/accordion/Accordion.stories.ts
@@ -1,13 +1,11 @@
-
 import { AccordionComponent } from './accordion/accordion.component';
-
 
 import { applicationConfig, Meta } from '@storybook/angular';
 import { importProvidersFrom } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import {AccordionContentDirective} from "./accordion-content.directive";
-import {AccordionHeadComponent} from "./accordion-head/accordion-head.component";
-import {AccordionItemComponent} from "./accordion-item/accordion-item.component";
+import { AccordionContentDirective } from './accordion-content.directive';
+import { AccordionHeadComponent } from './accordion-head/accordion-head.component';
+import { AccordionItemComponent } from './accordion-item/accordion-item.component';
 import { IconSpriteDirective } from '../icon/icon-sprite.directive';
 
 export default {
@@ -17,19 +15,19 @@ export default {
       providers: [importProvidersFrom(HttpClientModule)],
     }),
   ],
-  argTypes:{
-    viewType:{
-      options:['rounded','brick','circle'],
-      control:{type:'select'},
+  argTypes: {
+    viewType: {
+      options: ['rounded', 'brick', 'circle'],
+      control: { type: 'select' },
     },
-    showToggle:{
-      options:[true,false],
-      control:{type:'radio'}
-    }
+    showToggle: {
+      options: [true, false],
+      control: { type: 'radio' },
+    },
   },
-  args:{
-    viewType:'circle',
-    showToggle:true
+  args: {
+    viewType: 'circle',
+    showToggle: true,
   },
   component: AccordionComponent,
   moduleMetadata: {
@@ -39,37 +37,71 @@ export default {
 
 export const basicAccordion = (args) => ({
   moduleMetadata: {
-    imports: [AccordionComponent,AccordionContentDirective,AccordionItemComponent,AccordionHeadComponent,IconSpriteDirective],
+    imports: [
+      AccordionComponent,
+      AccordionContentDirective,
+      AccordionItemComponent,
+      AccordionHeadComponent,
+      IconSpriteDirective,
+    ],
   },
-  props:args,
+  props: args,
   template: `<div [tetaIconSprite]="'assets/icons.svg'" class="font-body-3 padding-3 bg-global-bgcard">
-      <teta-accordion>
-        <teta-accordion-item  [viewType]="viewType">
-          <teta-accordion-head [showToggle]="showToggle">
-            Heading 1
-          </teta-accordion-head>
-          <ng-template tetaAccordionContent>
-            <div class="padding-v-3 padding-h-2">I am the content 1</div>
-          </ng-template>
-        </teta-accordion-item>
-      </teta-accordion>
+         <h1 style="margin-bottom: 0.5em">Аккордион</h1>
+        <teta-accordion>
+             <teta-accordion-item [viewType]="viewType">
+              <teta-accordion-head [showToggle]="showToggle">Heading 1</teta-accordion-head>
+              <ng-template tetaAccordionContent>
+                <div class="padding-v-3 padding-h-2">I am the content 1</div>
+              </ng-template>
+            </teta-accordion-item>
+            <teta-accordion-item [viewType]="viewType">
+              <teta-accordion-head [showToggle]="showToggle">Heading 2</teta-accordion-head>
+              <ng-template tetaAccordionContent>
+                <div class="padding-v-3 padding-h-2">I am the content 1</div>
+              </ng-template>
+            </teta-accordion-item>
+            <teta-accordion-item [viewType]="viewType">
+              <teta-accordion-head [showToggle]="showToggle">Heading 3</teta-accordion-head>
+              <ng-template tetaAccordionContent>
+                <div class="padding-v-3 padding-h-2">I am the content 1</div>
+              </ng-template>
+            </teta-accordion-item>
+        </teta-accordion>
     </div>`,
 });
 export const disabledAccordion = (args) => ({
   moduleMetadata: {
-    imports: [AccordionComponent,AccordionContentDirective,AccordionItemComponent,AccordionHeadComponent,IconSpriteDirective],
+    imports: [
+      AccordionComponent,
+      AccordionContentDirective,
+      AccordionItemComponent,
+      AccordionHeadComponent,
+      IconSpriteDirective,
+    ],
   },
-  props:args,
+  props: args,
   template: `<div [tetaIconSprite]="'assets/icons.svg'" class="font-body-3 padding-3 bg-global-bgcard">
-      <teta-accordion>
-        <teta-accordion-item [disabled]="true"  [viewType]="viewType">
-          <teta-accordion-head [showToggle]="showToggle">
-            Heading 1
-          </teta-accordion-head>
-          <ng-template tetaAccordionContent>
-            <div class="padding-v-3 padding-h-2">I am the content 1</div>
-          </ng-template>
-        </teta-accordion-item>
-      </teta-accordion>
+        <h1 style="margin-bottom: 0.5em">Аккордион</h1>
+       <teta-accordion>
+             <teta-accordion-item [viewType]="viewType">
+              <teta-accordion-head>Heading 1</teta-accordion-head>
+              <ng-template tetaAccordionContent>
+                <div class="padding-v-3 padding-h-2">I am the content 1</div>
+              </ng-template>
+            </teta-accordion-item>
+            <teta-accordion-item [viewType]="viewType" [disabled]="true">
+              <teta-accordion-head>Heading 2</teta-accordion-head>
+              <ng-template tetaAccordionContent>
+                <div class="padding-v-3 padding-h-2">I am the content 1</div>
+              </ng-template>
+            </teta-accordion-item>
+            <teta-accordion-item [viewType]="viewType">
+              <teta-accordion-head>Heading 3</teta-accordion-head>
+              <ng-template tetaAccordionContent>
+                <div class="padding-v-3 padding-h-2">I am the content 1</div>
+              </ng-template>
+            </teta-accordion-item>
+        </teta-accordion>
     </div>`,
 });
