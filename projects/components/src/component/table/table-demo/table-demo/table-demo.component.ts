dc331ccc87155ed53e5b96ff09d6f74a1ec99234
Marat Sarvarov <shtirlizc@gmail.com>
2024-06-26 22:20:13 +0500

    fix: rowEditable to table demo

--- projects/components/src/component/table/table-demo/table-demo/table-demo.component.ts
+++ projects/components/src/component/table/table-demo/table-demo/table-demo.component.ts
@@ -1,14 +1,14 @@
-import {Component, HostListener, Input, OnInit, TemplateRef, ViewChild} from '@angular/core';
+import { Component, HostListener, Input, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import * as faker from 'faker';
-import {TableColumn} from '../../contract/table-column';
-import {FilterType} from '../../../filter/enum/filter-type.enum';
-import {IDictionary} from '../../../../common/contract/i-dictionary';
-import {IIdName} from '../../../../common/contract/i-id-name';
-import {EditType} from '../../enum/edit-type.enum';
-import {SelectType} from '../../enum/select-type.enum';
-import {EditEvent} from '../../enum/edit-event.enum';
-import {TableService} from '../../service/table.service';
-import {ICellInstance} from '../../contract/i-cell-instance';
+import { TableColumn } from '../../contract/table-column';
+import { FilterType } from '../../../filter/enum/filter-type.enum';
+import { IDictionary } from '../../../../common/contract/i-dictionary';
+import { IIdName } from '../../../../common/contract/i-id-name';
+import { EditType } from '../../enum/edit-type.enum';
+import { SelectType } from '../../enum/select-type.enum';
+import { EditEvent } from '../../enum/edit-event.enum';
+import { TableService } from '../../service/table.service';
+import { ICellInstance } from '../../contract/i-cell-instance';
 import { JsonPipe } from '@angular/common';
 import { TableComponent } from '../../table/table.component';
 import { IconComponent } from '../../../icon/icon/icon.component';
@@ -16,11 +16,11 @@ import { ButtonComponent } from '../../../button/button/button.component';
 import { ToolbarComponent } from '../../../toolbar/toolbar/toolbar.component';
 
 @Component({
-    selector: 'teta-table-demo',
-    templateUrl: './table-demo.component.html',
-    styleUrls: ['./table-demo.component.scss'],
-    standalone: true,
-    imports: [ToolbarComponent, ButtonComponent, IconComponent, TableComponent, JsonPipe]
+  selector: 'teta-table-demo',
+  templateUrl: './table-demo.component.html',
+  styleUrls: ['./table-demo.component.scss'],
+  standalone: true,
+  imports: [ToolbarComponent, ButtonComponent, IconComponent, TableComponent, JsonPipe],
 })
 export class TableDemoComponent implements OnInit {
   @Input() size: number;
@@ -28,24 +28,23 @@ export class TableDemoComponent implements OnInit {
   @Input() editType: EditType;
   @Input() selectType: SelectType;
   @Input() editEvent: EditEvent;
-  @ViewChild(TemplateRef, {read: TemplateRef, static: true}) dropdownTpl: TemplateRef<any>;
-  @ViewChild(TemplateRef, {read: TemplateRef, static: true}) contextMenu: TemplateRef<any>;
+  @ViewChild(TemplateRef, { read: TemplateRef, static: true }) dropdownTpl: TemplateRef<any>;
+  @ViewChild(TemplateRef, { read: TemplateRef, static: true }) contextMenu: TemplateRef<any>;
   tableService: TableService<any>;
   activeRow: any;
   selectedRows: any[];
   dict: IDictionary<IIdName<any>[]> = {
     ram: [
-      {id: 8, name: '8'},
-      {id: 16, name: '16'},
-      {id: 32, name: '32'},
-      {id: 64, name: '64'},
-      {id: 128, name: '128'},
+      { id: 8, name: '8' },
+      { id: 16, name: '16' },
+      { id: 32, name: '32' },
+      { id: 64, name: '64' },
+      { id: 128, name: '128' },
     ],
-    long: []
+    long: [],
   };
   data: any[] = [];
   columns = [];
-
 
   constructor() {
     this.dict['long'] = this.getLong();
@@ -73,19 +72,21 @@ export class TableDemoComponent implements OnInit {
         filterType: FilterType.string,
         headDropdownConfig: {
           strategy: 'replace',
-          tabs: [{
-            template: this.dropdownTpl,
-            icon: 'fx',
-            order: 15,
-            showTab: () => true
-          }]
-        }
+          tabs: [
+            {
+              template: this.dropdownTpl,
+              icon: 'fx',
+              order: 15,
+              showTab: () => true,
+            },
+          ],
+        },
       }),
       new TableColumn({
         name: 'city',
         filterType: FilterType.string,
         locked: true,
-        editable: false
+        editable: false,
       }),
       new TableColumn({
         name: 'date',
@@ -140,13 +141,14 @@ export class TableDemoComponent implements OnInit {
         name: `${faker.name.firstName()} ${faker.name.lastName()}`,
         date: faker.date.between(new Date(2010, 0, 1), new Date(2021, 0, 1)),
         long: faker.helpers.randomize([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
-        value: faker.datatype.number({min: 0, max: 100}),
-        summary: faker.datatype.number({min: 0, max: 100000}),
+        value: faker.datatype.number({ min: 0, max: 100 }),
+        summary: faker.datatype.number({ min: 0, max: 100000 }),
         ram: faker.helpers.randomize([8, 16, 32, 64, 128]),
         address: faker.address.streetAddress(),
         state: faker.address.state(),
         city: faker.address.city(),
         zip: faker.address.zipCode(),
+        editable: Math.random() > 0.1,
       });
     }
     return res;
@@ -164,32 +166,29 @@ export class TableDemoComponent implements OnInit {
   };
 
   cellEditStart(event: ICellInstance<any>) {
-
+    //
   }
 
   addRow() {
+    //
   }
 
   delete(selectedRows: any[]) {
     this.data = this.data.filter((row) => {
-      return selectedRows.findIndex(_ => _ === row) < 0;
-    })
+      return selectedRows.findIndex((_) => _ === row) < 0;
+    });
   }
 
   deleteAll() {
-
+    //
   }
 
   copy() {
-    navigator.clipboard.writeText(
-      this.toClipboardString([this.activeRow], this.tableService.getVisibleColumns())
-    );
+    navigator.clipboard.writeText(this.toClipboardString([this.activeRow], this.tableService.getVisibleColumns()));
   }
 
   copyAll() {
-    navigator.clipboard.writeText(
-      this.toClipboardString(this.selectedRows, this.tableService.getVisibleColumns())
-    );
+    navigator.clipboard.writeText(this.toClipboardString(this.selectedRows, this.tableService.getVisibleColumns()));
   }
 
   async paste() {
@@ -202,9 +201,7 @@ export class TableDemoComponent implements OnInit {
       (res: string, currentRow: any, i: number) =>
         `${res}${i === 0 ? '' : '\n'}${columns.reduce(
           (columnResult: string, column: TableColumn, j: number) =>
-            `${columnResult}${j === 0 ? '' : '\t'}${
-              currentRow[column.name] ?? ''
-            }`,
+            `${columnResult}${j === 0 ? '' : '\t'}${currentRow[column.name] ?? ''}`,
           ''
         )}`,
       ''
@@ -213,16 +210,11 @@ export class TableDemoComponent implements OnInit {
 
   private fromClipboard(data: string, columns: TableColumn[]) {
     const rows = data.split('\n').filter((_) => _?.length > 0);
-    const result = rows.map((_) =>
-      _.replace('\r', '').replace('\n', '').split('\t')
-    );
+    const result = rows.map((_) => _.replace('\r', '').replace('\n', '').split('\t'));
     return result.map((row: string[]) =>
       row.reduce((res, item, index) => {
         let value: any = item;
-        if (
-          columns[index]?.filterType === FilterType.number ||
-          columns[index]?.filterType === FilterType.list
-        ) {
+        if (columns[index]?.filterType === FilterType.number || columns[index]?.filterType === FilterType.list) {
           value = parseFloat(item);
         }
         if (columns[index]?.filterType === FilterType.boolean) {
@@ -234,5 +226,13 @@ export class TableDemoComponent implements OnInit {
         return res;
       }, {})
     );
+  }
+
+  rowEditable(row: any) {
+    return row.editable;
+  }
+
+  rowClass(row: any) {
+    return row.editable ? '' : 'table-row_disabled';
   }
 }
