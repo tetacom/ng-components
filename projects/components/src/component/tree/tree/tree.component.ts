42615a830620dd5af3579d96d659061d33fc38d9
Bulat.V <fclm.man@gmail.com>
2025-02-13 02:20:34 +0400

    fix: tree signals

--- projects/components/src/component/tree/tree/tree.component.ts
+++ projects/components/src/component/tree/tree/tree.component.ts
@@ -1,19 +1,23 @@
 import {
   ChangeDetectionStrategy,
-  ChangeDetectorRef,
   Component,
   ContentChild,
   ElementRef,
   EventEmitter,
   HostBinding,
+  input,
   Input,
   NgZone,
   OnChanges,
   OnDestroy,
   OnInit,
   Output,
-  SimpleChanges,
   ViewChild,
+  AfterViewInit,
+  computed,
+  effect,
+  model,
+  inject,
 } from '@angular/core';
 import { ITreeData } from '../../../common/contract/i-tree-data';
 import { TreeService } from '../tree.service';
@@ -25,94 +29,83 @@ import { ScrollableDirective } from '../../../directive/scrollable/scrollable.di
 import { ScrollableComponent } from '../../../directive/scrollable/scrollable/scrollable.component';
 
 @Component({
-    selector: 'teta-tree',
-    templateUrl: './tree.component.html',
-    styleUrls: ['./tree.component.scss'],
-    providers: [TreeService],
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    imports: [
-        ScrollableComponent,
-        CdkVirtualScrollViewport,
-        CdkFixedSizeVirtualScroll,
-        ScrollableDirective,
-        CdkVirtualForOf,
-        TreeItemComponent,
-    ]
+  selector: 'teta-tree',
+  templateUrl: './tree.component.html',
+  styleUrls: ['./tree.component.scss'],
+  providers: [TreeService],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [
+    ScrollableComponent,
+    CdkVirtualScrollViewport,
+    CdkFixedSizeVirtualScroll,
+    ScrollableDirective,
+    CdkVirtualForOf,
+    TreeItemComponent,
+  ],
 })
-export class TreeComponent implements OnInit, OnChanges, OnDestroy {
-  @Input()
-  set data(data: ITreeData[]) {
-    this._data = data;
-  }
+export class TreeComponent implements OnInit, OnChanges, OnDestroy, AfterViewInit {
+  private treeService = inject(TreeService);
+  private elementRef = inject(ElementRef);
+  private ngZone = inject(NgZone);
 
-  get data() {
-    return this._data;
-  }
+  data = input<ITreeData[]>([]);
+  padding = input<number>(8);
+  childNodeName = input<string>('children');
+  virtual = input<boolean>(false);
+  height = input<number>(28);
 
-  @Input() padding = 8;
-  @Input() childNodeName = 'children';
-  @Input() virtual: boolean;
-  @Input() height = 28;
-
-  @Input()
-  set openItems(items: ITreeData[]) {
-    this._openItems = items;
-    this._service.setOpenItems(items);
-  }
+  openItems = model<ITreeData[]>([]);
 
   @Output() service: EventEmitter<TreeService> = new EventEmitter<TreeService>();
-  @Output() openItemsChange = new EventEmitter();
 
   @ViewChild(CdkVirtualScrollViewport, { static: false }) viewport: CdkVirtualScrollViewport;
 
   @ContentChild(TetaTemplateDirective, { static: true })
   template: TetaTemplateDirective;
-  childPadding: boolean;
-  displayData: ITreeData[];
 
-  private _data: ITreeData[];
-  private _openItems: ITreeData[];
+  childPadding = computed(() => {
+    return this.data()?.some((_) => _[this.childNodeName()]?.length > 0);
+  });
+
+  displayData = computed(() => {
+    return this.getDisplayData(this.data(), 0);
+  });
+
   private _alive = true;
   private _obs: ResizeObserver;
 
-  constructor(
-    private _service: TreeService,
-    private _elementRef: ElementRef,
-    private _cdr: ChangeDetectorRef,
-    private _zone: NgZone,
-  ) {
-    this._service.openItems
+  constructor() {
+    effect(() => {
+      this.treeService.setOpenItems(this.openItems());
+    });
+    this.treeService.openItems
       .pipe(
         takeWhile((_) => this._alive),
-        filter((_) => this._openItems !== _),
+        filter((_) => this.openItems() !== _),
       )
       .subscribe((_: ITreeData[]) => {
-        this._openItems = _;
-        this.displayData = this.getDisplayData(this._data, 0);
-        this.openItemsChange.emit(_);
+        this.openItems.set(_);
         this.viewport?.checkViewportSize();
       });
   }
 
   @Input()
   set compareItems(func: (item: ITreeData) => any) {
-    this._service.compareItems = func;
+    this.treeService.compareItems = func;
   }
 
   get compareItems() {
-    return this._service.compareItems;
+    return this.treeService.compareItems;
   }
 
   @HostBinding('class.tree') private readonly treeClass = true;
 
   ngOnInit(): void {
-    this.service.emit(this._service);
+    this.service.emit(this.treeService);
     this.addResizeObserver();
   }
 
-  ngOnChanges(changes: SimpleChanges): void {
-    this.displayData = this.getDisplayData(this._data, 0);
-    this.childPadding = this.hasChildren(this._data);
+  ngOnChanges(): void {
     this.viewport?.checkViewportSize();
   }
 
@@ -122,8 +115,8 @@ export class TreeComponent implements OnInit, OnChanges, OnDestroy {
   }
 
   ngAfterViewInit() {
-    this._service.scrollToIndex.pipe(takeWhile(() => this._alive)).subscribe((index) => {
-      this._zone.runOutsideAngular(() => {
+    this.treeService.scrollToIndex.pipe(takeWhile(() => this._alive)).subscribe((index) => {
+      this.ngZone.runOutsideAngular(() => {
         setTimeout(() => {
           this.viewport?.scrollToIndex(index, 'smooth');
         });
@@ -138,20 +131,16 @@ export class TreeComponent implements OnInit, OnChanges, OnDestroy {
     return index;
   };
 
-  private hasChildren(data: ITreeData[]): boolean {
-    return data?.some((_) => _[this.childNodeName]?.length > 0);
-  }
-
   private getDisplayData(data: ITreeData[], level: number) {
     const result: ITreeData[] = [];
     data?.forEach((item: ITreeData) => {
       item['level'] = level;
       result.push(item);
       if (
-        item[this.childNodeName]?.length > 0 &&
-        this._openItems?.find((openItem) => this.compareItems(openItem) === this.compareItems(item))
+        item[this.childNodeName()]?.length > 0 &&
+        this.openItems()?.find((openItem) => this.compareItems(openItem) === this.compareItems(item))
       ) {
-        result.push(...this.getDisplayData(item[this.childNodeName], level + 1));
+        result.push(...this.getDisplayData(item[this.childNodeName()], level + 1));
       }
     });
     return result;
@@ -162,11 +151,11 @@ export class TreeComponent implements OnInit, OnChanges, OnDestroy {
       this.viewport?.checkViewportSize();
     });
 
-    this._obs.observe(this._elementRef.nativeElement);
+    this._obs.observe(this.elementRef.nativeElement);
   }
 
   private removeResizeObserver() {
-    this._obs.unobserve(this._elementRef.nativeElement);
+    this._obs.unobserve(this.elementRef.nativeElement);
     this._obs.disconnect();
   }
 }
