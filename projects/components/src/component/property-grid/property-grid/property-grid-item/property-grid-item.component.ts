0aed72dccf2480391c102f58d75c3c6027abe588
shtirlizc <shtirlizc@gmail.com>
2025-03-13 18:58:43 +0500

    fix: update property-grid hints

--- projects/components/src/component/property-grid/property-grid/property-grid-item/property-grid-item.component.ts
+++ projects/components/src/component/property-grid/property-grid/property-grid-item/property-grid-item.component.ts
@@ -95,6 +95,9 @@ export class PropertyGridItemComponent<T> implements OnDestroy, OnChanges {
   });
 
   hint = computed(() => {
+    if (this.column().hint === this.column().caption) {
+      return '';
+    }
     return `${this.column().hint || this.column().caption}${this.column().unit ? `, ${this.column().unit}` : ''}`;
   });
 
