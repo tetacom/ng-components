3c0f81df6afb6eccda49ab358ac2a70ab7f55e3f
Bulat.V <fclm.man@gmail.com>
2025-03-18 23:02:35 +0400

    fix: disable control directive

--- projects/components/src/directive/disable-control/disable-control.directive.ts
+++ projects/components/src/directive/disable-control/disable-control.directive.ts
@@ -1,12 +1,25 @@
-import { Directive, Input } from '@angular/core';
+import { Directive, Input, OnInit } from '@angular/core';
 import { NgControl } from '@angular/forms';
 
 @Directive({
   selector: '[tetaDisableControl]',
   standalone: true,
 })
-export class DisableControlDirective {
+export class DisableControlDirective implements OnInit {
+  private disabled: boolean;
+
   @Input() set tetaDisableControl(val: boolean) {
+    this.toggleControl(val);
+    this.disabled = val;
+  }
+
+  constructor(private ngControl: NgControl) {}
+
+  ngOnInit() {
+    this.toggleControl(this.disabled);
+  }
+
+  private toggleControl(val: boolean) {
     if (this.ngControl.control) {
       if (val) {
         this.ngControl.control.disable({
@@ -19,6 +32,4 @@ export class DisableControlDirective {
       }
     }
   }
-
-  constructor(private ngControl: NgControl) {}
 }
