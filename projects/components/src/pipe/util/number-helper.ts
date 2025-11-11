13f976bfca52146a017ff8aa91ae00b1c0a49536
shtirlizc <shtirlizc@gmail.com>
2025-11-11 16:00:20 +0500

    fix: keep zeros after the decimal point

--- projects/components/src/pipe/util/number-helper.ts
+++ projects/components/src/pipe/util/number-helper.ts
@@ -17,17 +17,31 @@ export const formatNumber = (
   chunkDelimiter: string,
   decimalDelimiter: string,
   chunkLength: number,
-) => {
-  const abs = Math.abs(value);
+): string => {
+  const numberValue = Number(value);
+  const abs = Math.abs(numberValue);
   if (0 < abs && 1 > abs) {
     const firstDigitIndex = Math.floor(Math.abs(Math.log10(abs)));
     decimalLength += firstDigitIndex;
   }
-  const precision = Math.min(getPrecision(value), Math.floor(decimalLength));
-  const result = '\\d(?=(\\d{' + chunkLength + '})+' + (precision > 0 ? '\\D' : '$') + ')';
-  const num = value.toFixed(precision);
-  return (decimalDelimiter ? num.replace('.', decimalDelimiter) : num).replace(
-    new RegExp(result, 'g'),
+
+  let precision = Math.min(getPrecision(numberValue), Math.floor(decimalLength));
+  // Пытаемся сохранить количество нулей после запятой для 0
+  if (numberValue === 0 && typeof value === 'string') {
+    const decimalPart = value.split('.')[1] || '';
+    const trailingZerosMatch = decimalPart.match(/0*$/);
+    precision = trailingZerosMatch ? trailingZerosMatch[0].length : 0;
+  }
+
+  const digitGroupingRegex = '\\d(?=(\\d{' + chunkLength + '})+' + (precision > 0 ? '\\D' : '$') + ')';
+  let formattedNumberString = numberValue.toFixed(precision);
+  // Учитываем знак минуса для отрицательного нуля
+  if (numberValue === 0 && 1 / numberValue === -Infinity) {
+    formattedNumberString = '-' + formattedNumberString;
+  }
+
+  return (decimalDelimiter ? formattedNumberString.replace('.', decimalDelimiter) : formattedNumberString).replace(
+    new RegExp(digitGroupingRegex, 'g'),
     '$&' + chunkDelimiter,
   );
 };
