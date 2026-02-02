75a76d7654b2a43ff1e065457bc76441eef0fdd0
Bulat.V <fclm.man@gmail.com>
2026-02-02 15:01:56 +0400

    fix: selection cell

--- projects/components/src/component/table/selection-cell/selection-cell.component.ts
+++ projects/components/src/component/table/selection-cell/selection-cell.component.ts
@@ -2,52 +2,55 @@ import {
   ChangeDetectionStrategy,
   ChangeDetectorRef,
   Component,
+  computed,
   HostBinding,
-  Input,
-  OnDestroy,
-  OnInit,
+  input,
+  Signal,
 } from '@angular/core';
 import { TableService } from '../service/table.service';
-import { takeWhile } from 'rxjs/operators';
 import { FormsModule } from '@angular/forms';
 import { CheckboxComponent } from '../../checkbox/checkbox/checkbox.component';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
-    selector: 'teta-selection-cell',
-    templateUrl: './selection-cell.component.html',
-    styleUrls: ['./selection-cell.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    imports: [CheckboxComponent, FormsModule]
+  selector: 'teta-selection-cell',
+  templateUrl: './selection-cell.component.html',
+  styleUrls: ['./selection-cell.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [CheckboxComponent, FormsModule],
 })
-export class SelectionCellComponent<T> implements OnInit, OnDestroy {
-  @Input() row: T;
+export class SelectionCellComponent<T> {
+  row = input.required<T>();
 
   @HostBinding('class.cell') private readonly tableCellClass = true;
-  selectedRows: T[] = [];
 
-  private _alive = true;
+  selectedRows: Signal<T[]>;
+
+  isSelected: Signal<boolean>;
 
   constructor(
     private _svc: TableService<T>,
     private _cdr: ChangeDetectorRef,
-  ) {}
+  ) {
+    this.selectedRows = toSignal(this._svc.selectedRows, {
+      initialValue: [],
+    });
 
-  selectRow(value: boolean) {
-    if (value) {
-      this._svc.selectRow(this.row);
-    } else {
-      this._svc.deselectRow(this.row);
-    }
-  }
-
-  ngOnInit(): void {
-    this._svc.selectedRows.pipe(takeWhile((_) => this._alive)).subscribe((_) => {
-      this.selectedRows = _;
-      this._cdr.markForCheck();
+    this.isSelected = computed(() => {
+      const selectedRows = this.selectedRows();
+      const row = this.row();
+      if (row && selectedRows) {
+        return selectedRows.indexOf(row) >= 0;
+      }
+      return false;
     });
   }
 
-  ngOnDestroy(): void {
-    this._alive = false;
+  selectRow(value: boolean) {
+    if (value) {
+      this._svc.selectRow(this.row());
+    } else {
+      this._svc.deselectRow(this.row());
+    }
   }
 }
